60a3a6374151645e0497e835ee1f8e60e3eee5ee
Sara Menezes <sara.menezes1303@gmail.com>
2022-10-26 18:22:27 -0300

    Primeira versão da calculadora

---
+++ cal.js
@@ -0,0 +1,18 @@
+function soma(a,b){
+   
+}
+function multiplicacao (a,b){
+    
+}
+function subtracao(a,b){
+    
+}
+function divisao (a,b){
+   
+}
+module.exports = {
+    soma;
+    multiplicacao;
+    subtracao;
+    divisao;
+};
